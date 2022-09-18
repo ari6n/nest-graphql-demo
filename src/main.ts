34c3b3431f45bf42d91f29577a99f7caaf321236
ogolovko <ogolovko@gmail.com>
2022-09-18 16:33:40 +0600

    Overwritten project with working nest-sample 23-graphql-code-first

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const GRAPHQL_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(GRAPHQL_PORT, () => console.log(
+    `GraphQL is now running on: http://localhost:${GRAPHQL_PORT}/graphql`
+  ));
+  // console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
